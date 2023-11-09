b504a36d5dfeccfefc4e215584d72c9f57a388ed
Kali <tavarescauanf@gmail.com>
2023-11-08 22:32:14 -0300

    Correcoes das tabelas

--- banco.sql
+++ banco.sql
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
------ Cria��o do Banco -----
-=======
 ----- Criação do Banco -----
->>>>>>> master
 
 CREATE DATABASE InterComm
 GO
@@ -10,28 +6,7 @@ GO
 USE InterComm
 GO
 
-<<<<<<< HEAD
------ Cria��o das Tabelas -----
-
--- Incerto se usaremos:
-
-CREATE TABLE Responsaveis (
-	IdResponsavel	int		primary key		identity,
-	Nome			varchar(60)	not null
-)
-
-CREATE TABLE Telefones (
-	IdResponsavel	int		primary key		identity,
-	Numero			varchar(20)	not null
-)
-
-CREATE TABLE Emails (
-	IdResponsavel	int		primary key		identity,
-	Endereco		varchar(30)	not null
-)
-=======
 ----- Criaçãoo das Tabelas -----
->>>>>>> master
 
 -------------------------------------------------------
 
@@ -39,45 +14,21 @@ CREATE TABLE Locais (
 	IdLocal				int		primary key		identity,
 	LocalNomeFantasia	varchar(60),
 	LocalRazaoSocial	varchar(60)		not null,
-<<<<<<< HEAD
-	CNPJ				varchar(20)		not null, -- conferir quantos caracteres depois
-	TipoLocal			int				not null,
-	CEP					varchar(8),
-	Rua					varchar(30)		not null,
-=======
 	CNPJ				varchar(14)		not null, -- conferir quantos caracteres depois
 	TipoLocal			int				not null,
 	ANTT				varchar(30),	
 	IE					varchar(30),
 	CEP					varchar(8),
 	Logradouro			varchar(30)		not null,
->>>>>>> master
 	Bairro				varchar(30),
 	Cidade				varchar(30)		not null,
 	Estado				varchar(30)		not null,
 	Numero				int,
 	Complemento			varchar(30),
-<<<<<<< HEAD
-
-	ANTT				varchar(30),
-	IE					varchar(30),
-
-	-- Isso resolve o problema por agora
-	-- Responsaveis		varchar(300),
-	-- Telefones			varchar(300),
-	-- Emails				varchar(300),
-	ResponsaveisCod		int foreign key references Responsaveis(IdResponsavel)
-=======
->>>>>>> master
 )
 
 CREATE TABLE Motoristas (
 	IdMotorista		int		primary key		identity,
-<<<<<<< HEAD
-	MotoristaNome	varchar(60) not null,
-	CPF				varchar(60),
-	CNH				varchar(60) not null,
-=======
 	NomeMotorista	varchar(60) not null,
 	CPF				varchar(11),
 	CNH				varchar(10) not null,
@@ -86,65 +37,39 @@ CREATE TABLE Motoristas (
 CREATE TABLE Locais_Motoristas (
 	CodMotorista	int 	foreign key		references Motoristas(IdMotorista),
 	CodLocal		int		foreign key		references Locais(IdLocal)
->>>>>>> master
+	primary key		(CodMotorista, CodLocal)
 )
 
 CREATE TABLE Conjuntos (
 	IdConjunto		int		primary key		identity, -- verificar campos e rela��es depois
-<<<<<<< HEAD
-	CodMotorista	int	foreign key references Motoristas(IdMotorista),
-	Placa			varchar(7)	not null,
-=======
 	CodMotorista	int		foreign key 	references Motoristas(IdMotorista),
 	CodPertence		int,
 	Pertence 		int,
 	PlacaA			varchar(7)	not null,
 	PlacaB			varchar(7),
 	PlacaC			varchar(7),
->>>>>>> master
 )
 
 CREATE TABLE Contratos (
 	IdContrato		int		primary key		identity,
 	CodLocal		int		foreign key references Locais(IdLocal) not null,
 	CodCommodity	int		not null,
-<<<<<<< HEAD
-	Volume			float	not null,
-	VolumeAtual		float	default 0,
-	VolumePendente	float	default 0,
-	Status			int		default 0
-=======
 	DataInicio		date 	not null,
 	Volume			float	not null,
 	VolumeAtual		float	default 0,
 	VolumePendente	float	default 0,
 	ValorUnitario	float 	not null,
 	Status			int		default 0,
->>>>>>> master
 )
 
 CREATE TABLE OrdensDeCarregamento (
 	IdOrdem				int		primary key		identity,
-<<<<<<< HEAD
-	CodMotorista		int	foreign key references Motoristas(IdMotorista) not null,
-	CodConjunto			int	foreign key references Conjuntos(IdConjunto) not null,
-	CodContrato			int	foreign key references Contratos(IdContrato) not null,
-	CodTransportadora	int	foreign key references Locais(IdLocal) not null,
-	CodDestino			int foreign key references Locais(IdLocal)	not null,
-	Status				int	default 0,
-
-	-- Isso resolve o problema por agora
-	PlacaA				varchar(7)	not null,
-	PlacaB				varchar(7),
-	PlacaC				varchar(7),
-=======
 	CodMotorista		int		foreign key references Motoristas(IdMotorista) not null,
 	CodConjunto			int		foreign key references Conjuntos(IdConjunto) not null,
 	CodContrato			int		foreign key references Contratos(IdContrato) not null,
 	CodTransportadora	int		foreign key references Locais(IdLocal) not null,
 	CodDestino			int 	foreign key references Locais(IdLocal)	not null,
 	Status				int		default 0,
->>>>>>> master
 )
 
 CREATE TABLE NotaFiscal (
@@ -152,8 +77,6 @@ CREATE TABLE NotaFiscal (
 	CodOrdem	int		foreign key references OrdensDeCarregamento(IdOrdem) not null
 )
 
-<<<<<<< HEAD
-=======
 
 ----- Tabelas Complementares -----
 
@@ -177,7 +100,6 @@ CREATE TABLE Emails (
 )
 
 
->>>>>>> master
 ----- Cria��o das Views -----
 
 -- CREATE VIEW vwOrdem
@@ -189,11 +111,7 @@ CREATE TABLE Emails (
 	
 -- 		from Motorista M, Conjunto Conj, Contrato Cont, Transportadora T, Usuario U
 
-<<<<<<< HEAD
-CREATE VIEW vwContrato
-=======
 CREATE VIEW vw_Contrato
->>>>>>> master
 as
 	select 
 		Contratos.IdContrato, Contratos.CodCommodity, Contratos.Volume, Contratos.VolumeAtual, Contratos.VolumePendente, Contratos.Status,
@@ -202,8 +120,4 @@ as
 		Contratos
 	inner join Locais ON Contratos.CodLocal = Locais.IdLocal
 
-<<<<<<< HEAD
 select * from vwContrato
-=======
-select * from vwContrato
->>>>>>> master
